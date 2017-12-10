dc9a2776fe3a6536e4d6b440fe3a304aa11e3f4e
clinyong <clinyong@gmail.com>
2017-12-10 13:24:13 +0800

    add opt quick sort(only need one list to do quick sort)

--- sort/quick.js
+++ sort/quick.js
@@ -1,8 +1,6 @@
 const { list } = require("./constants");
 const { swap } = require("./utils");
 
-let total = 0;
-
 function quickSort(list) {
 	if (list.length <= 1) return list;
 
@@ -12,7 +10,6 @@ function quickSort(list) {
 
 	const pivot = list[0];
 	list.forEach(item => {
-		total += 1;
 		if (item > pivot) {
 			moreList.push(item);
 		} else if (item < pivot) {
@@ -25,17 +22,36 @@ function quickSort(list) {
 	return [...quickSort(lessList), ...pivotList, ...quickSort(moreList)];
 }
 
-// TODO:
-function partition(list, left, right, pivotIndex) {
-	const pivotValue = list[pivotIndex];
-    swap(a[pivotIndex], a[right]);
+function partition(l, left, right) {
+	const pivotValue = l[right];
 
-    const storeIndex = left;
-    for (let i = left; i < right; i++) {
-    }
+	let storeIndex = left;
+	// move all values to left, which are below than pivotValue
+	// i should start from left, not left+1
+	for (let i = left; i < right; i++) {
+		if (l[i] < pivotValue) {
+			if (storeIndex !== i) {
+				swap(l, storeIndex, i);
+			}
+			storeIndex += 1;
+		}
+	}
+
+	swap(l, right, storeIndex);
+	return storeIndex;
 }
 
-function quickSortOpt1(params) {}
+function quickSortOpt1(l, left, right) {
+	if (right > left) {
+		const pivotIndex = partition(l, left, right);
+		quickSortOpt1(l, left, pivotIndex - 1);
+		quickSortOpt1(l, pivotIndex + 1, right);
+	}
+}
 
-console.log(quickSort(list));
-console.log(total);
+console.log(quickSort(list.slice()));
+
+const list2 = list.slice();
+
+quickSortOpt1(list2, 0, list2.length - 1);
+console.log(list2);
